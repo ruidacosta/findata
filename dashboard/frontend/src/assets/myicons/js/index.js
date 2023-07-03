d10601e63026def913db227dd632ea05e792165e
ruidacosta <ruiduartecosta@gmail.com>
2023-07-03 23:18:14 +0100

    Delete old files;

--- dashboard/frontend/src/assets/myicons/js/index.js
+++
@@ -1,7 +0,0 @@
-import { icnCommodity } from './icons/icn-comodity.js'
-import { icnIndex } from './icons/icn-index.js'
-import { icnStockMarket } from './icons/icn-stock-market.js'
-
-export { icnCommodity }
-export { icnIndex }
-export { icnStockMarket }
